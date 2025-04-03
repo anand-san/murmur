f43305ba0731b59e168a24c0bdf8d41f3bab3930
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2025-04-03 11:37:21 +0200

    feat(global-shortcut): track shortcut key

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,8 +7,34 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            #[cfg(desktop)]
+            {
+                use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
+
+                let shortcut_key = Shortcut::new(Some(Modifiers::SHIFT), Code::Backquote);
+                app.handle().plugin(
+                    tauri_plugin_global_shortcut::Builder::new().with_handler(move |_app, shortcut, event| {
+                        if shortcut == &shortcut_key {
+                            match event.state() {
+                              ShortcutState::Pressed => {
+                                println!("Shortcut Pressed!");
+                              }
+                              ShortcutState::Released => {
+                                println!("Shortcut Released!");
+                              }
+                            }
+                        }
+                    })
+                    .build(),
+                )?;
+
+                app.global_shortcut().register(shortcut_key)?;
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
